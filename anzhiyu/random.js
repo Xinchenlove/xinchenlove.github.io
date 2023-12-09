833c039e2719c4342d566b6ed94b0aa26efb16f4
Xinchen <a1413024330@163.com>
2023-12-10 03:10:29 +0800

    Site updated: 2023-12-10 03:10:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/09/c++ English/","2023/12/09/leetcode/","2023/12/09/c++问题笔记/"];function toRandomPost(){
+var posts=["2023/12/09/c++ English/","2023/12/10/c++问题笔记/","2023/12/10/leetcode/","2023/12/10/image/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
