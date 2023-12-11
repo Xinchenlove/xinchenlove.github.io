8f91bc6c71a88e56f37feed0085c592adffc430f
Xinchen <a1413024330@163.com>
2023-12-11 15:34:20 +0800

    Site updated: 2023-12-11 15:34:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/09/c++ English/","2023/12/10/c++问题笔记/","2023/12/10/leetcode/","2023/12/10/image/"];function toRandomPost(){
+var posts=["2023/12/10/2023年回顾-最喜欢的一集/","2023/12/10/c++问题笔记/","2023/12/09/c++ English/","2023/12/10/image/","2023/12/10/leetcode/","2023/12/10/2023年总结/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
