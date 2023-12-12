c95bc054c337e3b26ec411259854011f3e37fa8a
Xinchen <a1413024330@163.com>
2023-12-12 23:34:00 +0800

    Site updated: 2023-12-12 23:33:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/10/2023年回顾-最喜欢的一集/","2023/12/10/2023年总结/","2023/12/09/c++ English/","2023/12/11/数据结构/","2023/12/10/leetcode/","2023/12/10/c++问题笔记/","2023/12/10/image/"];function toRandomPost(){
+var posts=["2023/12/10/2023年总结/","2023/12/10/2023年回顾-最喜欢的一集/","2023/12/12/HTML-CSS学习笔记2/","2023/12/12/HTML_CSS学习笔记/","2023/12/09/c++ English/","2023/12/10/c++问题笔记/","2023/12/10/leetcode/","2023/12/10/image/","2023/12/11/单链表-数组模拟/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
